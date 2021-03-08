601f8061d8466835af1450ae58768272edb18683
chungman <cndaks23@gmail.com>
2021-03-08 15:59:21 +0900

    Study1

---
+++ Lecture1(2021.03.08)/src/kr/ac/kopo/lecture1/LottoMain.java
@@ -0,0 +1,12 @@
+package kr.ac.kopo.lecture1;
+
+public class LottoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LottoUtil Util = new LottoUtil();
+		Util.todayLotto();
+	}
+
+}
