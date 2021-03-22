291a0feccddd09782c0fb07a3e9f6f704224328b
Kim-chungman <79954014+Kim-chungman@users.noreply.github.com>
2021-03-22 16:03:15 +0900

    Delete abs01 directory

--- abs01/LGPrinter.java
+++
@@ -1,9 +0,0 @@
-package Java0322.UserMain.abs01;
-
-public class LGPrinter extends Printer{
-
-	public void lgPrint() {
-		System.out.println("LG프린터에서 출력중...");
-	}
-	
-}
--- abs01/SamsungPrinter.java
+++
@@ -1,9 +0,0 @@
-package Java0322.UserMain.abs01;
-
-public class SamsungPrinter extends Printer{
-
-	public void samPrint() {
-		System.out.println("삼성프린터에서 출력중...");
-	}
-	
-}
